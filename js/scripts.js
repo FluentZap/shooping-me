80173ccbea33e88294908121b4110a9c2c6472d9
megan schulte and Nathan Aden <nathan@>
2019-04-01 14:21:32 -0700

    iterate through the list items using a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
-var favorites_list = [];
-var prompts = ["Favorite food?", "Favorite drink?", "Favorite animal?"];
-var index = 0;
+var listindex = 0;
 $(document).ready(function () {
-  $("#SelectedThing").text(prompts[index]);
-
-  $("#form-favorite").submit(function (event) {
+  addListItem();
+  $("#questions").submit(function (event) {
     event.preventDefault();
 
-    favorites_list.push($("#form-favoriteThing").val());
-    $("#form-favoriteThing").val("");
+    for (i = listindex; i > 0; --i) {
+      $("#finalList").prepend(
+        "<li>" +
+        $("#inputGroup" + i + " input").val() +
+        "</li>"
+      );
+    };
 
-    index++;
-    $("#SelectedThing").text(prompts[index]);
+    $("#finalList").fadeIn();
+  });
 
-    if (index > prompts.length - 1)
-    {
-      $("#questions").fadeOut(function () {
-        favorites_list.forEach(function(favorite) {
-          $("#favorite-display-list").append("<li>" + favorite + "</li>");
-        });
-
-        $("#answers").fadeIn();
-      });
-
-    }
-
+  $("#addItem").click(function (event) {
+    addListItem();
   });
 });
+
+
+function addListItem () {
+  $("#form-list").prepend(
+  '<div id="inputGroup' + listindex++ + '" class="form-group">' +
+  $("#form-list #inputGroup").html() + '</div>');
+  console.log(
+  '<div id="inputGroup' + listindex + '" class="form-group">' +
+  $("#form-list #inputGroup").html() + '</div>');
+
+}
